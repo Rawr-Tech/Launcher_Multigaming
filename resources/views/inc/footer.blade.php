81fae387d5bd82e5538107730a8ff1c4a72987cb
Wirk <contact@wirk>
2017-04-11 20:37:57 +0200

    Fix

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <footer class="main">
 
-    &copy; {{ \Carbon\Carbon::now()->format('Y') }} <strong>{{ config('app.name') }}</strong> | Launcher Admin Control by <a href="http://youtube.com/MrDarkSkil" target="_blank">MrDarkSkil</a>
+    &copy; {{ \Carbon\Carbon::now()->format('Y') }} <strong>{{ config('app.name') }}</strong> | Launcher Admin Control by <a href="http://youtube.com/MrDarkSkil" target="_blank">MrDarkSkil</a> and <a href="https://github.com/iWirk" target="_blank">Wirk</a>
 
 </footer>
